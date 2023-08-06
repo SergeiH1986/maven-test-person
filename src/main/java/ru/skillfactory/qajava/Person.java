d78163b9f8314a31657f519aa8f50741b5b63c0a
SergeiH1986 <zwark2005god@mail.ru>
2023-08-06 15:19:36 +0500

    first commit

---
+++ src/main/java/ru/skillfactory/qajava/Person.java
@@ -0,0 +1,15 @@
+package ru.skillfactory.qajava;
+
+public class Person {
+    public static boolean isTeenager(int age)
+    {
+        boolean result;
+        if (age < 19) {
+            result = true;
+        }
+        else {
+            result = false;
+        }
+        return result;
+    }
+}
